fd73527d8c4a8a2f19e8e53d95296c246c02ad0b
Maria Belan-Crawford <44656740+mViolet@users.noreply.github.com>
2021-02-25 23:46:49 -0600

    make tic-tac-toe grid

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
+const letters = document.querySelectorAll("span")
 
+letters.forEach(letter => letter.addEventListener('mousedown', showLetter))
+
+let counter = 0
+
+function showLetter(){
+    if (counter % 2 === 0) {
+        this.innerText = 'O'
+        this.style.color = 'black'
+        counter++
+    } else {
+        this.innerText = 'X'
+        this.style.color = 'black'
+        counter++
+    }
+}
